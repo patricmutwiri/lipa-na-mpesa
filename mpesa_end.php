0d5c2c6abce44e6c0f59c5db83a6f99c0e94d2c0
Patrick Mutwiri <patricmutwiri@users.noreply.github.com>
2016-01-21 14:40:16 +0300

    Update mpesa_end.php
    version

--- mpesa_end.php
+++ mpesa_end.php
@@ -1,9 +1,9 @@
 <?php
 /**
  * @package	HikaShop for Joomla!
- * @version	2.5.0
- * @author	gbc
- * @copyright	(C) 2010-2015 HIKARI SOFTWARE. All rights reserved.
+ * @version	1
+ * @author	patrick
+ * @copyright	(C) 2010-2015 patrick SOFTWARE. All rights reserved.
  * @license	GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  */
 defined('_JEXEC') or die('Restricted access');
