e3f0d0afa1e6550e9e1b58591d82ee9569fb9b29
Patrick Mutwiri <patricmutwiri@users.noreply.github.com>
2016-01-22 12:14:26 +0300

    Update mpesa_save.php
    Save Mpesa Code to the order table too.

--- mpesa_save.php
+++ mpesa_save.php
@@ -5,7 +5,7 @@ define( 'JPATH_BASE', realpath(dirname(__FILE__)));
 define( 'DS', DIRECTORY_SEPARATOR ); 
 require_once ( JPATH_BASE .DS.'includes'.DS.'defines.php' ); 
 require_once ( JPATH_BASE .DS.'includes'.DS.'framework.php' ); 
-$mainframe =& JFactory::getApplication('site'); 
+$mainframe = JFactory::getApplication('site'); 
 $mainframe->initialise(); 
 $app 	=	JFactory::getApplication();
 //we have the framework now
@@ -22,12 +22,19 @@ $app 	=	JFactory::getApplication();
 		$mpesa->name = $user->name;
 		$mpesa->email = $user->email;
 		if($db->insertObject('#__mpesa',$mpesa,'id')) {
-			$app->redirect('index.php','Payment Successfull. ');
-			echo  ' Payment Successfull ';
+			$order = stdClass();
+			$order->order_id   = $vars->get('ORDERID');
+			$order->mpesa_code = $vars->get('mpesa_code');
+			if( $db->updateObject('#__hikashop_order',$order,'order_id') ){
+				$app->redirect('index.php','Payment saved Successfully. ');
+				echo  ' Payment saving Successfull ';
+			} else{
+				$app->redirect('index.php','Payment not saved. Contact Support');
+			}			
 		} else {
 			echo  ' Error ';
 		}
 	} else{
-
+		$app->redirect('index.php');
 	}
 ?>
