e208b084909b3bc7f92e47673f810a8e1709448a
Patrick Mutwiri <patricmutwiri@users.noreply.github.com>
2016-01-22 10:10:29 +0300

    Update mpesa_save.php
    check if code exists before saving

--- mpesa_save.php
+++ mpesa_save.php
@@ -1,5 +1,4 @@
 <?php 
-// Get Joomla! framework 
 define( '_JEXEC', 1 ); 
 define( '_VALID_MOS', 1 ); 
 define( 'JPATH_BASE', realpath(dirname(__FILE__))); 
@@ -8,10 +7,11 @@ require_once ( JPATH_BASE .DS.'includes'.DS.'defines.php' );
 require_once ( JPATH_BASE .DS.'includes'.DS.'framework.php' ); 
 $mainframe =& JFactory::getApplication('site'); 
 $mainframe->initialise(); 
-?>
-<?php 
-//save mpesa code
-		$vars = JFactory::getApplication()->input;
+$app 	=	JFactory::getApplication();
+//we have the framework now
+	//save mpesa code
+	$vars = JFactory::getApplication()->input;
+	if(!empty($vars->get('mpesa_code'))){ //only if mpesa code is found
 		$user  = JFactory::getUser();
 		$db    = JFactory::getDBO();
 		$mpesa = new stdClass();
@@ -22,8 +22,12 @@ $mainframe->initialise();
 		$mpesa->name = $user->name;
 		$mpesa->email = $user->email;
 		if($db->insertObject('#__mpesa',$mpesa,'id')) {
+			$app->redirect('index.php','Payment Successfull. ');
 			echo  ' Payment Successfull ';
 		} else {
 			echo  ' Error ';
 		}
+	} else{
+
+	}
 ?>
